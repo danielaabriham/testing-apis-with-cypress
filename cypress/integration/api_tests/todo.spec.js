b349a20e001a5dd2d2b26d8ee623916df514151f
Daniela Abriham <daniela.abriham@xoomworks.local>
2022-01-26 20:25:25 +0200

    add methods

--- cypress/integration/api_tests/todo.spec.js
+++ cypress/integration/api_tests/todo.spec.js
@@ -1,12 +1,30 @@
-const cypress = require("cypress");
-
-describe(`TODO api testing`, () => {
+describe("TODO api testing", () => {
   let todoItem;
-  it("fetches Todo items - GET req", () => {
-    cypress.request("/todos").as("todoRequest");
-    cypress.get("@todoRequest").then((todos) => {
+
+  it("fetches Todo items - GET", () => {
+    cy.request("/todos/").as("todoRequest");
+    cy.get("@todoRequest").then((todos) => {
       expect(todos.status).to.eq(200);
       assert.isArray(todos.body, "Todos Response is an array");
+    });
+  });
+
+  it("deletes Todo items - DELETE", () => {
+    cy.request(`/todos/${todoItem}`).as("todoRequest");
+    cy.get("@todoRequest").then((todos) => {
+      expect(todos.status).to.eq(200);
+      assert.isArray(todos.body, "todo deleted!");
+    });
+  });
+
+  it("Adds Todo item - POST", () => {
+    cy.request("POST", "/todos/", { task: "run tests" }).as("todoRequest");
+    cy.get("@todoRequest").then((todos) => {
+      expect(todos.status).to.eq(200);
+      cy.wrap(todos.body).should("deep.include", {
+        task: "run tests",
+        completed: false,
+      });
     });
   });
 });
